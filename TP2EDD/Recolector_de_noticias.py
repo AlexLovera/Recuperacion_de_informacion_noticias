f2d2385d9881da1ae78da44f4def10e8c16a1d32
Alexx <alexander.lovera8@hotmail.com>
2020-08-08 23:49:34 -0300

    Actualiza Recolector_de_noticias a ultima version.  Agrega excepciones, ademas de codigo para el manejo de carpetas y archivos

--- TP2EDD/Recolector_de_noticias.py
+++ TP2EDD/Recolector_de_noticias.py
@@ -1,9 +1,10 @@
 import xml.etree.ElementTree as xml
 import configparser
 import os.path
-from urllib import request
+from colorama import init,Style;init()
 import requests
-color_error="\033[4;31m"
+from Excepciones_TP2 import Tipo_de_archivo_incorrecto_exception,Directorio_no_encontrado
+color_error="\033[4;31m"+ Style.BRIGHT
 color_blanco='\033[0;37m'
 def _compara_xmls_y_añade_articulos_nuevos(url,xmlfile):
     # print(url)#debuggear, porque se "trababa", el problema es que la pagina tardaba en responder, y despues se cayo
@@ -41,44 +42,37 @@ def _compara_xmls_y_añade_articulos_nuevos(url,xmlfile):
     except Exception as e:
         # print(Exception.with_traceback())
         if "not well-formed" in e.__str__():
-            print(color_error+"Estructura de la pagina mal formada o pagina caida")
-        print(url+color_blanco)
+            print(color_error+f"Estructura de la pagina mal formada o pagina caida: {url}")
+        print(color_error+"Error en: ",url+color_blanco)
 
-def _crear_xml_mediante_config_y_agrega_articulo(config):
+def _crear_xml_mediante_config_y_agrega_articulo(carpeta_medios,config):
+    if not os.path.isdir(carpeta_medios):#si no existe la carpeta medios devuelvo excepcion propia
+        raise Directorio_no_encontrado(carpeta_medios)
     configuracion = configparser.ConfigParser()
     configuracion.read(config)
+    cantidad_de_items_a_saltear=len(configuracion.items(configuracion.default_section))
     for medio in configuracion.sections():
+        if not os.path.isdir(f"{carpeta_medios}/{medio}"):#si no existe la carpeta para el medio, se la creo
+            os.mkdir(f"{carpeta_medios}/{medio}")
         lista_de_tupla_items_de_la_seccion=configuracion.items(medio)
         url_base=str(configuracion[medio]["url_base"])
         numero_de_tupla = 0
-        #tengo que ignorar las primeras tuplas? son items del default
         for tupla_de_valores in lista_de_tupla_items_de_la_seccion:
             variable,valor=tupla_de_valores
             numero_de_tupla+=1
             # url_base_como_variable=[k for k, v in configuracion[seccion] if v == medio?]
-            ## for variable,valor in tupla_de_valores: parece que no funciona en tuplas
-            if(numero_de_tupla<=4):
-                continue#pasa los valores por defecto
-            if numero_de_tupla>4:
-                seccion=str(valor)
-                # print(f"Medios/{medio}/{str(variable) + '.xml'}")
-                # print(not os.path.exists(f"Medios/{medio}/{str(variable) + '.xml'}"))
-            # print(url_base + seccion)#compruebo que imprima bien el url
-            # if not os.path.isdir(f"Medios/{medio}"):#PARA CREAR LA CARPETA PARA EL MEDIO SI NO EXISTE
-            #     print('Creando carpeta para el medio');
-            #     os.mkdir(f"Medios/{medio}")
+            if(numero_de_tupla<=cantidad_de_items_a_saltear+1): continue#4#pasa los valores por defecto
+            if numero_de_tupla>cantidad_de_items_a_saltear+1: seccion=str(valor)#4
             if not os.path.exists(f"Medios/{medio}/{str(variable) + '.xml'}"):#para saltear los valor default y url
                 #si no existe el archivo, lo tengo que crear
                 resp = requests.get(url_base + seccion)
                 with open(f"Medios/{medio}/{variable + '.xml'}", 'wb') as archivo:
                     archivo.write(resp.content)
             elif os.path.exists(f"Medios/{medio}/{str(variable) + '.xml'}"):
-                #no hacer nada? ejecutar el programa de recoleccion?
-                # print(f"Medios/{medio}/{str(variable) + '.xml'}")
                 _compara_xmls_y_añade_articulos_nuevos(url_base + seccion,f"Medios/{medio}/{str(variable) + '.xml'}")
 
-def recolectar_articulos_segun_config(config):
+def recolectar_articulos_segun_config(carpeta_medios,config):
     try:
-        _crear_xml_mediante_config_y_agrega_articulo(config)
+        _crear_xml_mediante_config_y_agrega_articulo(carpeta_medios,config)
     except configparser.ParsingError:
         print('\033[4;31m' + f"No se encontró el archivo a parsear: {config}")
