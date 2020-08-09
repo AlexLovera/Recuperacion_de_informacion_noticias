424756d009327fbf769fcd165fc3685a5b14c924
Alexx <alexander.lovera8@hotmail.com>
2020-08-08 23:52:46 -0300

    Actualiza Indice_invertido_y_compresion a ultima version. Borra comentarios y agrega funcion para revertir saltos de docID

--- TP2EDD/Indice_invertido_y_compresion.py
+++ TP2EDD/Indice_invertido_y_compresion.py
@@ -44,23 +44,27 @@ def _generar_saltos_docID(indice_sin_comprimir):#MODIFICADO
             else:
                 #aca le resto el valor previo en la lista de apariciones...
                 indice_con_saltos_docID[termIDi].append(term_post_list[docIDi] - indice_sin_comprimir[termIDi][docIDi - 1])
-    print(indice_con_saltos_docID)
-    import time
-    time.sleep(10)
     return indice_con_saltos_docID
 
+def revertir_saltos_docID(posting_list_con_saltos):
+    for docIDi in range(len(posting_list_con_saltos)):
+        if docIDi != 0:
+            valor_actual=posting_list_con_saltos[docIDi]
+            posting_list_con_saltos[docIDi]=valor_actual+posting_list_con_saltos[docIDi-1]
+    return posting_list_con_saltos
+
+
+
 def _comprimir_posting_list(ruta,indice_final):
-    # indice_sin_comprimir = BSBI_algorithm(ruta)
-    indice_con_saltos_docID=_generar_saltos_docID(BSBI_algorithm(ruta))
+    indice_sin_comprimir = BSBI_algorithm(ruta)
+    indice_con_saltos_docID=_generar_saltos_docID(indice_sin_comprimir)
     with open("posting_list_comprimida", "wb") as archivo:
             for termIDi in indice_con_saltos_docID:
                 lista_docs=indice_con_saltos_docID[termIDi]
-                # termi_posting_decodificada=codificador.decode(lista_docs)
                 #codifico posting list con Variable byte code
                 termi_posting_codificada=Variable_byte_encode_list(lista_docs)
                 indice_final[termIDi]=(archivo.tell(),len(termi_posting_codificada),sys.getsizeof(termi_posting_codificada))
                 archivo.write(termi_posting_codificada)
-    print("indice_final_posting_comprimida: ",indice_final)
     return indice_final
 
 
@@ -68,13 +72,6 @@ def crear_indice_invertido(ruta,posting_list_comprimida):#ruta y boolean
     indice_final = dict()
     if posting_list_comprimida:#supongo que lo tengo que escribir, lista comprimida?
         indice_final=_comprimir_posting_list(ruta,indice_final)
-        # posting_list = BSBI_algorithm(ruta)#ejemplo sin comprimir
-        # with open("posting_list_comprimida","wb") as archivo:
-        #     for termIDi in posting_list:
-        #         lista_docs=posting_list[termIDi]
-        #         indice_final[termIDi]=(archivo.tell(),len(codificador.decode(lista_docs)),sys.getsizeof(lista_docs))
-        #         archivo.write(lista_docs)
-        # print("indice_final_posting_comprimida: ",indice_final)
     else:
         indice_sin_comprimir = BSBI_algorithm(ruta)
         for termIDi in indice_sin_comprimir:
@@ -84,7 +81,3 @@ def crear_indice_invertido(ruta,posting_list_comprimida):#ruta y boolean
     with open("indice_invertido_pos","w") as archivo:
         json.dump(indice_final,archivo)
     return indice_final
-
-# generar_saltos_docID(ruta_creacion_posting_list="Medios_prueba")
-#4251: [19, -19, 0, 0, 23, -23, 0, 0, 165, -165, 0, 0, 41, -41, 0, 0, 42, -42, 0, 0, 171, --171, 0, 0, 172, -172, 0, 0, 45, -45, 0, 0, 175, -175, 0, 0, 177, -177, 0, 0, 179, -179, 0, 0, 180, -180, 0, 0, 181, -181, 0, 0,
-# print(codificador.decode(posting_list_sin_comprimir[4251]))
